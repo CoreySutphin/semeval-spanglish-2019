98e6e3c853e5b83f33267daa8a1299d1d0fabc2c
Corey <corey@thinknimble.com>
2019-10-05 09:30:39 -0400

    Adding spanish and english stop words, modifying conll to csv script to remove stop words and convert emojis

--- scripts/preprocessing/parse_conll_to_csv.py
+++ scripts/preprocessing/parse_conll_to_csv.py
@@ -3,13 +3,15 @@
     the tokens.
     Created for the 2019 SemEval Challenge.
 
-    Args: CONLL file (1), remove_users (2): y or n, remove_lang (3): y or n.
+    Args: CONLL file (1), remove_users (2): y or n, remove_lang (3): y or n,
+          remove_stop_words(4): y or n, convert_emojis(5) y or n
 """
 
 import sys
 import os
 import re
 import pandas as pd
+import emoji
 
 assert len(sys.argv) > 1, "Must pass in CONLL file to convert"
 
@@ -17,6 +19,14 @@ assert len(sys.argv) > 1, "Must pass in CONLL file to convert"
 conll_file = sys.argv[1]
 remove_users = True if sys.argv[2].lower() == "y" else False
 remove_lang = True if sys.argv[3].lower() == "y" else False
+remove_stop_words = True if sys.argv[4].lower() == 'y' else False
+convert_emojis = True if sys.argv[5].lower() == 'y' else False
+
+if remove_stop_words:
+    with open('spanish-stopwords.txt') as spanish_file:
+        stopwords_es = [line.rstrip('\n') for line in spanish_file]
+    with open('english-stopwords.txt') as english_file:
+        stopwords_en = [line.rstrip('\n') for line in english_file]
 
 label_counts = {}
 tweets = []
@@ -41,14 +51,21 @@ with open(conll_file, 'r') as file:
         # Create a list of objects representing the tokens
         for token in lines[1:]:
             word, language = token.split('\t')
-            if word.startswith('@'):
+            if remove_stop_words:
+                if word in stopwords_en or word in stopwords_es:
+                    continue
 
+            if convert_emojis:
+                word = emoji.demojize(word)
+
+            if word.startswith('@'):
                 if remove_users:
-                    word = "USER"
+                    word = "@"
+
             if remove_lang:
                 tweet.append(word)
             else:
-                tweet.append((word,language))
+                tweet.append((word, language))
         tweets.append(tweet)
 
 print(label_counts)
