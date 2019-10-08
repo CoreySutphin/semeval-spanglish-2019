5aceb5981a601b27cb9669c8aa90909edccfe63a
Cove Soyars <soyarsc@vcu.edu>
2019-10-08 12:11:34 -0400

    bash

--- runit.sh
+++ runit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-word_emb_path = scripts/word_embeddings_CNN.py
-char_emb_path = initial_CNN/initial_CNN.py
+
 echo Running word embedding CNN...
-python $word_emb_path
+python scripts/word_embeddings_CNN.py
