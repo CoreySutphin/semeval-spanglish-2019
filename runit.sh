362b87ed073842415ef99b7d04556a3b9c5f571e
covesoyars <41693982+covesoyars@users.noreply.github.com>
2019-10-08 13:37:28 -0400

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -2,7 +2,7 @@
 # Author: Cove Soyars
 python -m venv semeval
 source semeval/bin/activate
-pip install -r requirements.txt
+semeval/bin/pip install -r requirements.txt
 
 echo Running char embedding CNN...
 cd initial_CNN
